e01054b78f57db2f73d051e7e065cf8253943c34
Lewis Horwood <horwood.lewis@gmail.com>
2015-12-17 21:43:53 -0700

    Number input working. Operators next!

--- calculator.js
+++ calculator.js
@@ -14,17 +14,91 @@ Look a bit like a calculator if possible*/
 var numberA = null;
 var numberB = null;
 var currentResult = 0;
+//Has an operator been pressed?
+var operator = false;
+//Was an operator the last key pressed?
+var operatorLast = false;
+//Was a decimal the last key pressed?
+var decimalLast = false;
 
 
-var displayResult = function(){
-	$("#screen").text(currentResult.toString().slice(0, 19));
+var display = function(n){
+	$("#screen").text(n.toString().slice(0, 19));
 }
 
-$("#buttonClear").click(function(){
+var numKey = function(x){
+	if (x === "." && decimalLast){
+		return;
+	}
+	if (! numberA){
+		if (x === "."){
+			numberA = "0.";
+		} else {
+			decimalLast = false;
+			numberA = x;
+		}
+		display(numberA);
+	} else if (operatorLast){
+		if (x === "."){
+			numberB = "0.";
+		} else {
+			decimalLast = false;
+			numberB = x;
+		}
+		operatorLast = false;
+		display(numberB);
+	} else if (numberB && operator){
+		numberB = numberB.toString() + x.toString();
+		display(numberB);
+	} else {
+		numberA = numberA.toString() + x.toString();
+		display(numberA);
+	}
+}
+
+$("#btn-clear").click(function(){
 	currentResult = 0;
-	displayResult();
+	numberA = null;
+	numberB = null;
+	operator = false;
+	display(currentResult);
+});
+
+$("#btn-1").click(function(){
+	numKey(1);
+});
+$("#btn-2").click(function(){
+	numKey(2);
+});
+$("#btn-3").click(function(){
+	numKey(3);
+});
+$("#btn-4").click(function(){
+	numKey(4);
+});
+$("#btn-5").click(function(){
+	numKey(5);
+});
+$("#btn-6").click(function(){
+	numKey(6);
+});
+$("#btn-7").click(function(){
+	numKey(7);
+});
+$("#btn-8").click(function(){
+	numKey(8);
+});
+$("#btn-9").click(function(){
+	numKey(9);
+});
+$("#btn-0").click(function(){
+	numKey(0);
+});
+$("#btn-decimal").click(function(){
+	numKey(".");
+	decimalLast = true;
 });
 
 $(document).ready(function(){
-	displayResult();
+	display(currentResult);
 });
